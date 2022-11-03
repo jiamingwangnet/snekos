094d2462ccc714cea7ec1dda947a786b009c7964
Jiaming Wang <jiamingwang.net@gmail.com>
2022-11-03 11:48:05 +1100

    print test

--- src/boot/main.asm
+++ src/boot/main.asm
@@ -1,9 +1,11 @@
-global _start
+global start
 
 section .text ; program
 bits 32
-_start:
+start:
     ; entry
+    mov word [0xb8000], 0x0248 ; move H to video memory (0xb8000)
+    hlt
 
 section .bss ; uninitialised data
 
