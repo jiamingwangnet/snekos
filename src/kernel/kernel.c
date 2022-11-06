fb9d60c19e11999aa225617864dd7dad1d591787
jiamingwangnet <jiamingwang.net@gmail.com>
2022-11-06 17:02:16 +1100

    testing framebuffer

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -17,18 +17,17 @@ void kernel_main()
     //     }
     // }
 
-    
-    if(multiboot_info & 7)
+    if (multiboot_info & 7)
     {
-        for(unsigned int i = 0xA0000; i < 0xAFFFF; i++)
+        for (unsigned int i = 0xA0000; i < 0xAFFFF; i++)
         {
-            *(uint32_t*)i = 255;
+            *(uint32_t *)i = 255;
         }
     }
 
     unsigned long addr = multiboot_info & 0xffffffff;
 
-    unsigned size = *(unsigned *) addr;
+    unsigned size = *(unsigned *)addr;
     struct multiboot_tag *tag;
 
     char str[15];
@@ -37,10 +36,9 @@ void kernel_main()
     serial_char('\n');
     serial_char('\n');
 
-    for (tag = (struct multiboot_tag *) (addr + 8); // loop though every tag idk how it works
-       tag->type != MULTIBOOT_TAG_TYPE_END;
-       tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
-                                       + ((tag->size + 7) & ~7)))
+    for (tag = (struct multiboot_tag *)(addr + 8); // loop though every tag idk how it works
+         tag->type != MULTIBOOT_TAG_TYPE_END;
+         tag = (struct multiboot_tag *)((multiboot_uint8_t *)tag + ((tag->size + 7) & ~7)))
     {
         char tagtype[15];
         itoa(tag->type, tagtype, 10);
@@ -54,21 +52,22 @@ void kernel_main()
         serial_str(tagsize);
         serial_char('\n');
 
-        if(tag->type == MULTIBOOT_TAG_TYPE_FRAMEBUFFER)
+        if (tag->type == MULTIBOOT_TAG_TYPE_FRAMEBUFFER)
         {
             serial_char('\n');
             multiboot_uint32_t color;
             unsigned i;
-            struct multiboot_tag_framebuffer *tagfb
-              = (struct multiboot_tag_framebuffer *) tag;
-            void *fb = (void *) (unsigned long) tagfb->common.framebuffer_addr;
-            
+            struct multiboot_tag_framebuffer *tagfb = (struct multiboot_tag_framebuffer *)tag;
+            volatile void *fb = (void *)(unsigned long long)tagfb->common.framebuffer_addr;
+
+
+#pragma region 
             serial_str("framebuffer address: 0x");
             char fbaddr[15];
-            itoa(tagfb->common.framebuffer_addr, fbaddr, 16);
+            itoa((unsigned long)tagfb->common.framebuffer_addr, fbaddr, 16);
             serial_str(fbaddr);
             serial_char('\n');
-            
+
             serial_str("width: ");
             char fbwidth[15];
             itoa(tagfb->common.framebuffer_width, fbwidth, 10);
@@ -87,8 +86,52 @@ void kernel_main()
             serial_str(fbbpp);
             serial_char('\n');
 
+            serial_str("fb type: ");
+            char fbtype[15];
+            itoa(tagfb->common.framebuffer_type, fbtype, 10);
+            serial_str(fbtype);
+            serial_char('\n');
+
+            serial_str("blue mask size: ");
+            char bmsize[15];
+            itoa(tagfb->framebuffer_blue_mask_size, bmsize, 10);
+            serial_str(bmsize);
+            serial_char('\n');
+
+            serial_str("blue field pos: ");
+            char bfp[15];
+            itoa(tagfb->framebuffer_blue_field_position, bfp, 10);
+            serial_str(bfp);
+            serial_char('\n');
+
+            serial_char('\n');
+#pragma endregion
+            color = ((1 << tagfb->framebuffer_blue_mask_size) - 1)
+                    << tagfb->framebuffer_blue_field_position;
+
+            // uint32_t* pixel = fb + 0;
+            uint64_t value = fb;
+
+            serial_str("fb pixel[0]: 0x");
+
+            char fbpixel[15];
+            itoa(value, fbpixel, 16);
+            char fbpixeldec[15];
+            itoa(value, fbpixeldec, 10);
+
+            serial_str(fbpixel);
+            serial_str(" (");
+            serial_str(fbpixeldec);
+            serial_str(")");
+            
+            serial_char('\n');
             serial_char('\n');
             
+            // for (i = 0; i < tagfb->common.framebuffer_width && i < tagfb->common.framebuffer_height; i++)
+            // {
+            //     multiboot_uint32_t *pixel = fb + tagfb->common.framebuffer_pitch * i + 4 * i;
+            //     *pixel = color;
+            // }
         }
     }
 }
