229097a50e29b9ecdbdd39184fdaafce20ca0326
jiamingwangnet <jiamingwang.net@gmail.com>
2022-11-13 20:59:22 +1100

    draw rect

--- src/kernel/include/graphics.h
+++ src/kernel/include/graphics.h
@@ -4,3 +4,4 @@
 #define FRAMBUFFER 0x3D000000
 
 void put_pixel(uint32_t x, uint32_t y, Color color, framebuffer_tag* fbtag);
+void draw_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, Color color, framebuffer_tag* fbtag);
--- src/kernel/include/impl/graphics.c
+++ src/kernel/include/impl/graphics.c
@@ -2,8 +2,20 @@
 
 void put_pixel(uint32_t x, uint32_t y, Color color, framebuffer_tag* fbtag)
 {
-    uint8_t* addr = (uint8_t*)(FRAMBUFFER + y * fbtag->common.framebuffer_pitch + x);
-    addr[0] = color.b;
-    addr[1] = color.g;
-    addr[2] = color.r;
+    uint8_t* addr = (uint8_t*)(FRAMBUFFER + y * fbtag->common.framebuffer_pitch + (fbtag->common.framebuffer_bpp / 8 * x));
+    
+    addr[fbtag->framebuffer_red_field_position / fbtag->framebuffer_red_mask_size] = color.r;
+    addr[fbtag->framebuffer_green_field_position / fbtag->framebuffer_green_mask_size] = color.g;
+    addr[fbtag->framebuffer_blue_field_position / fbtag->framebuffer_blue_mask_size] = color.b;
+}
+
+void draw_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, Color color, framebuffer_tag* fbtag)
+{
+    for(uint32_t ry = y; ry < height; ry++)
+    {
+        for(uint32_t rx = x; x < width; rx++)
+        {
+            put_pixel(rx, ry, color, fbtag);
+        }
+    }
 }
