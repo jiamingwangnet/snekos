1a510edfea101007431996445ee146e348ee94b0
jiamingwangnet <jiamingwang.net@gmail.com>
2022-11-13 21:03:59 +1100

    draw_rect bugfix

--- src/kernel/include/impl/graphics.c
+++ src/kernel/include/impl/graphics.c
@@ -11,9 +11,9 @@ void put_pixel(uint32_t x, uint32_t y, Color color, framebuffer_tag* fbtag)
 
 void draw_rect(uint32_t x, uint32_t y, uint32_t width, uint32_t height, Color color, framebuffer_tag* fbtag)
 {
-    for(uint32_t ry = y; ry < height; ry++)
+    for(uint32_t ry = y; ry < height + y; ry++)
     {
-        for(uint32_t rx = x; x < width; rx++)
+        for(uint32_t rx = x; rx < width + x; rx++)
         {
             put_pixel(rx, ry, color, fbtag);
         }
