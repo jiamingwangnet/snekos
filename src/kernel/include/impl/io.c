3a9352eae67c9542470b093b13279f6b02cf9ed4
jiamingwangnet <jiamingwang.net@gmail.com>
2022-11-20 03:23:47 +1100

    correction

--- src/kernel/include/impl/io.c
+++ src/kernel/include/impl/io.c
@@ -5,7 +5,7 @@ void out(uint16_t port, uint8_t data)
     __asm__("out %%dx, %%ax" :: "d"(port), "a"(data));
 }
 
-void in(uint16_t port)
+uint8_t in(uint16_t port)
 {
     uint8_t o;
     __asm__("in %%ax, %%dx" : "=a"(o) : "d"(port));
--- src/kernel/include/io.h
+++ src/kernel/include/io.h
@@ -2,6 +2,13 @@
 #include "types.h"
 
 #define COM1 0x3F8
+#define COM2 0x2F8
+#define COM3 0x3E8
+#define COM4 0x2E8
+#define COM5 0x5F8
+#define COM6 0x4F8
+#define COM7 0x5E8
+#define COM8 0x4E8
 
 void out(uint16_t port, uint8_t data);
-void in(uint16_t port);
+uint8_t in(uint16_t port);
