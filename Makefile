6cfc5ffeea45b2fba1350ba26062af59c91ab32c
jiamingwangnet <jiamingwang.net@gmail.com>
2022-12-10 22:41:22 +1100

    higher half kernel start

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ C_OBJ_FILES := $(patsubst src/kernel/%.c, build/kernel/%.o, $(C_SOURCE_FILES))
 
 $(C_OBJ_FILES): build/kernel/%.o : src/kernel/%.c
 	mkdir -p $(dir $@) && \
-	gcc -masm=intel -m64 -ffreestanding -c $(patsubst build/kernel/%.o, src/kernel/%.c, $@) -o $@
+	gcc -mcmodel=large -masm=intel -m64 -ffreestanding -c $(patsubst build/kernel/%.o, src/kernel/%.c, $@) -o $@
 
 $(ASM_OBJ_FILES): build/boot/%.o : src/boot/%.asm
 	mkdir -p $(dir $@) && \
@@ -19,6 +19,7 @@ build/assets/vga_font.o: assets/vga_font.psf
 
 .PHONY: build
 build: $(C_OBJ_FILES) $(ASM_OBJ_FILES) build/assets/vga_font.o
+	mkdir -p dist
 	ld --nmagic --output=build/kernel.bin --script=linker.ld $(C_OBJ_FILES) $(ASM_OBJ_FILES) build/assets/vga_font.o && \
 	cp build/kernel.bin bootloader/iso/boot && \
 	grub-mkrescue -o dist/SnekOS.iso bootloader/iso
--- src/boot/main.asm
+++ src/boot/main.asm
@@ -1,32 +1,42 @@
-global start
 global gdt64.data
 global multiboot_info
 global page_table_l2
 
 extern long_mode_start
+extern _kernel_start
+extern _kernel_end
 
 section .text ; program
+global start:function (start.end - start)
 bits 32
 start:
     cli
-
     ; entry
-    mov esp, stack_top ; setup stack pointer  
 
     call save_multiboot_info 
-
     call check_multiboot
     call check_cpuid
     call check_long_mode
 
     call setup_tables
-    call enable_paging 
+
+    mov eax, _kernel_start
+    or eax, 0b10000011
+    mov [page_table_l2 - 0xC0000000], eax
+
+    call enable_paging ; page fault
+    
+    lea ecx, [.higher_half]
+    jmp ecx
+
+.higher_half:
+    mov esp, stack_top ; setup stack pointer  
 
     ; grub starts protected mode which means it also loads its own gdt, however, grub's gdt should not be used
     lgdt [gdt64.pointer] ; load the gdt
 
     jmp gdt64.code:long_mode_start ; far jump
-
+.end:
 err:
     hlt
 
@@ -68,13 +78,13 @@ save_multiboot_info:
     ret
 
 setup_tables:
-    mov eax, page_table_l3
+    mov eax, (page_table_l3 - 0xC0000000)
     or eax, 0b11 ; present bit and writable bit
-    mov dword [page_table_l4 + 0], eax ; make page_table_l4 point to page_table_l3
+    mov dword [(page_table_l4 - 0xC0000000) + 0], eax ; make page_table_l4 point to page_table_l3
 
-    mov eax, page_table_l2
+    mov eax, (page_table_l2 - 0xC0000000)
     or eax, 0b11
-    mov dword [page_table_l3 + 0], eax
+    mov dword [(page_table_l3 - 0xC0000000) + 0], eax
 
     ; loop
     mov ecx, 0 ; ecx is the counter
@@ -82,7 +92,7 @@ setup_tables:
     mov eax, 0x200000  ; 2 MiB, eax is used for multiplication
     mul ecx ; muliplies ecx by eax and stores the result in eax
     or eax, 0b10000011 ; the first bit indicates that this page is very large (huge page bit)
-    mov [page_table_l2 + ecx * 8], eax ; write the page into the table
+    mov [(page_table_l2 - 0xC0000000) + ecx * 8], eax ; write the page into the table
 
     inc ecx
     cmp ecx, 512 ; loop 512 times
@@ -91,9 +101,9 @@ setup_tables:
     ret
 
 enable_paging:
-    mov eax, page_table_l4 ; cannot directly move to cr3
+    mov eax, (page_table_l4 - 0xC0000000) ; cannot directly move to cr3
     mov cr3, eax ; move the page table to cr3 (control register)
-
+    
     ; enable PAE
     mov eax, cr4 ; modify the contents of cr4
     or eax, 1 << 5 ; set the 5th bit
