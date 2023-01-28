b5c27ab7d48e601bf53edbb2f314f409c87660a1
jiamingwangnet <jiamingwang.net@gmail.com>
2023-01-28 22:52:38 +1100

    added another header

--- src/kernel/include/drivers/pci.h
+++ src/kernel/include/drivers/pci.h
@@ -28,6 +28,30 @@ typedef struct
     uint8_t bist;
 }   pci_common_t;
 
+typedef struct
+{
+    pci_common_t common;
+
+    uint32_t bar0;
+    uint32_t bar1;
+    uint32_t bar2;
+    uint32_t bar3;
+    uint32_t bar4;
+    uint32_t bar5;
+
+    uint32_t cardbus_cis_ptr;
+    uint16_t subsystem_vendor_id;
+    uint16_t subsystem_id;
+    uint32_t ex_rom_addr;
+    uint8_t capabilities_ptr;
+
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint8_t min_grant;
+    uint8_t max_latency;
+} pci_header0_t;
+
+
 uint16_t pci_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 
 uint16_t pci_read_vendor(uint8_t bus, uint8_t slot, uint8_t func);
